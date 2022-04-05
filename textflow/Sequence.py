80ea69192704c5756babb5a40299772e87a02f58
Jaime Collado <jcollado@ujaen.es>
2022-04-05 19:16:32 +0200

    Developing Sequence class

--- textflow/Sequence.py
+++ textflow/Sequence.py
@@ -16,9 +16,14 @@ class SequenceIterator:
 
 
 class Sequence:
-    def __init__(self, id, sequences):
-        self.id = id
-        self.sequences = sequences
+    def __init__(self, object):
+        # TODO: Extraer id y sequences a partir del object de cualquier forma que se nos ocurra
+        # ver: https://huggingface.co/docs/datasets/v2.0.0/en/package_reference/loading_methods#datasets.load_dataset
+        if isinstance(object, str):
+            self.id = object
+        else:
+            self.id = "collection"
+        self.sequences = ["subcollection_1", "subcollection_2", "subcollection_2"]
 
     def __str__(self):
         return f"id: {self.id}, sequences: {self.sequences}"
@@ -35,20 +40,22 @@ class Sequence:
     
     def __getitem__(self, i):
         if isinstance(i, str):
-            return "Str indexing is not supported yet"  # TODO
+            for sequence in self.sequences:
+                if isinstance(sequence, Sequence):
+                    if sequence.id == i: return sequence
+            raise ValueError(f"Sequence index '{i}' not found")
         elif isinstance(i, int):
             if i < 0:
                 i = len(self.sequences) + i
 
             if i >= len(self.sequences):
-                raise IndexError("Sequence index out of range")
+                raise IndexError(f"Sequence index '{i}' out of range")
             else:
                 return self.sequences[i]
-        else:
+        else:   # TODO: Support slices (e.g. [2:4])?
             invalid_type = type(i)
             raise TypeError(
-                "LockableList indices must be integers or slices, not {}"
-                .format(invalid_type.__name__)
+                f"Sequence indices must be integers or strings, not {invalid_type.__name__}"
             )
 
     def get_depth(self):
@@ -59,10 +66,14 @@ class Sequence:
 
 # TODO: Move these tests to the ./tests folder
 if __name__ == "__main__":
-    seq = Sequence(1, ["doc1", 3, Sequence(4, [2])])
-    print(seq)
-    print(seq[2])
-    print(seq["a"])
-    for s in seq:
+    sequence_from_folder = Sequence("../tests/data/doc_1") # File loading
+    sequence_from_file = Sequence("lorem ipsum") # String loading
+
+    sequence_from_folder.sequences.append(sequence_from_file)
+
+    print(sequence_from_folder)
+    print(sequence_from_folder[2])
+    print(sequence_from_folder["lorem ipsum"])
+    for s in sequence_from_folder:
         print(s)
-    print(seq[20])
+    print(sequence_from_folder[20]) # This should fail
