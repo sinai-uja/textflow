406dde5e96c60647d809c66c2c9a2cab3306332d
estrellaVallecillo <118640977+estrellaVallecillo@users.noreply.github.com>
2025-12-15 14:11:40 +0100

    Update SelAnalyzer.py

--- textflow/SelAnalyzer.py
+++ textflow/SelAnalyzer.py
@@ -27,7 +27,7 @@ class SelAnalyzer(Analyzer):
         self.selFile = pd.read_csv(pathFile).drop(['Unnamed: 0'],axis=1)
         self.tokenizer = tokenizer
 
-    def to_float(x):
+    def to_float(self,x):
         if isinstance(x, str):
             x = x.replace(',', '.')
         return float(x)
